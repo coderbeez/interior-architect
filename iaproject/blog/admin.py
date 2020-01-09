a36e15162a228350938d7de336591458b5791b22
coderbeez <nospam@firville.com>
2020-01-09 13:51:20 +0000

    Add blog models & admin setup.

--- iaproject/blog/admin.py
+++ iaproject/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post, Comment
 
 # Register your models here.
+#admin.site.register(Post)
+#admin.site.register(Comment)
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
+    #from docs
+
+admin.site.register(Post, PostAdmin)
--- iaproject/blog/models.py
+++ iaproject/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField(default=timezone.now)
+    image = models.ImageField(blank=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return f'<Name: {self.title}, ID: {self.id}>'
+        #name shown in admin
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+
+    #related names https://simpleisbetterthancomplex.com/tips/2018/02/10/django-tip-22-designing-better-models.html#naming-your-models
+
+
+
+
