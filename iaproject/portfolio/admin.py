317b32f38207bf24dc64ab132da891d9b7002130
coderbeez <nospam@firville.com>
2020-01-09 14:09:27 +0000

    Add portfolio models & admin setup.

--- iaproject/portfolio/admin.py
+++ iaproject/portfolio/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Project, Section
 
 # Register your models here.
+#admin.site.register(Post)
+#admin.site.register(Comment)
+
+class SectionInline(admin.TabularInline):
+    model = Section
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [SectionInline]
+    #from docs
+
+admin.site.register(Project, ProjectAdmin)
--- iaproject/portfolio/models.py
+++ iaproject/portfolio/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField(default=timezone.now)
+    image = models.ImageField(blank=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return f'<Name: {self.title}, ID: {self.id}>'
+        #name shown in admin
+
+class Section(models.Model):
+    project = models.ForeignKey('Project', on_delete=models.CASCADE, related_name='sections')
+    image = models.ImageField(blank=True)
+    content = models.TextField()
+
+
+    #related names https://simpleisbetterthancomplex.com/tips/2018/02/10/django-tip-22-designing-better-models.html#naming-your-models
+
