c3cf82e16fb66571d24ffd5d6a4244ae5b81b98b
coderbeez <nospam@firville.com>
2020-01-09 15:53:23 +0000

    Add cv models & admin setup.

--- iaproject/cv/admin.py
+++ iaproject/cv/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Role, Point
+
+# Register your models here.
+#admin.site.register(Post)
+#admin.site.register(Comment)
+
+class PointInline(admin.TabularInline):
+    model = Point
+
+class RoleAdmin(admin.ModelAdmin):
+    inlines = [PointInline]
+    #from docs
+
+admin.site.register(Role, RoleAdmin)
 
 # Register your models here.
--- iaproject/cv/models.py
+++ iaproject/cv/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Role(models.Model):
+    company = models.CharField(max_length=200)
+    link = models.CharField(max_length=200, blank=True)
+    title = models.CharField(max_length=200)
+    timeframe = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return f'<Name: {self.company} {self.title}, ID: {self.id}>'
+        #name shown in admin
+
+class Point(models.Model):
+    role = models.ForeignKey('Role', on_delete=models.CASCADE, related_name='points')
+    content = models.TextField()
+
+
+    #related names https://simpleisbetterthancomplex.com/tips/2018/02/10/django-tip-22-designing-better-models.html#naming-your-models
