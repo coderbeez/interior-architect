57f1c45b29dc9aab84e933d0754452b4c2346746
coderbeez <nospam@firville.com>
2020-01-10 08:47:01 +0000

    Add project view. Link views to db.

--- iaproject/portfolio/views.py
+++ iaproject/portfolio/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project, Section
 
 # Create your views here.
 def portfolio(request):
     #return HttpResponse('Portfolio List')
-    return render(request, 'portfolio/portfolio.html')   
+    projects = Project.objects.all().order_by('-date')
+    context = {'projects': projects}
+    return render(request, 'portfolio/portfolio.html', context)
+
+def project(request, pk):
+    project = Project.objects.get(pk=pk)
+    sections = Section.objects.filter(project=project)
+    context = {'project': project, 'sections': sections}
+    return render(request, 'portfolio/project.html', context)
+
