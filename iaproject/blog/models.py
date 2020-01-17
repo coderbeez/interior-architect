aaa000be899598ec16bfa557282000af27de2c79
coderbeez <nospam@firville.com>
2020-01-17 10:49:10 +0000

    Add name, date, reply fields to comment model.

--- iaproject/blog/models.py
+++ iaproject/blog/models.py
@@ -12,13 +12,13 @@ class Category(models.Model):
 
 
 class Blog(models.Model):
-    category = models.ForeignKey('Category', null=True, on_delete=models.CASCADE, related_name='blogs')
-    title = models.CharField(max_length=200, blank=True)
-    date = models.DateField(default=timezone.now)
-    image = models.ImageField(blank=True)
-    content = models.TextField(blank=True)
-    order = models.IntegerField(null=True)
-    exclude = models.BooleanField(default=False)
+    category = models.ForeignKey('Category', null=True, on_delete=models.CASCADE, related_name='blogs') # null ok !!!
+    title = models.CharField(max_length=200, blank=True) # char blank
+    date = models.DateField(default=timezone.now) # auto
+    image = models.ImageField(blank=True) # ?
+    content = models.TextField(blank=True) # text blank
+    order = models.IntegerField(null=True) # null ok - should not be required in form!!!
+    exclude = models.BooleanField(default=False) # yes no
 
     def __str__(self):
         return f'<Order: {self.order}, Name: {self.title}, ID: {self.id}>'
@@ -26,16 +26,20 @@ class Blog(models.Model):
 
 
 class Section(models.Model):
-    blog = models.ForeignKey('Blog', on_delete=models.CASCADE, related_name='sections')
-    title = models.CharField(max_length=500, blank=True)
-    image = models.ImageField(blank=True)
-    content = models.TextField(blank=True)
+    blog = models.ForeignKey('Blog', on_delete=models.CASCADE, related_name='sections') #gotta have
+    title = models.CharField(max_length=500, blank=True) # char blank
+    image = models.ImageField(blank=True) # ?
+    content = models.TextField(blank=True) # text blank
     
 
 
 class Comment(models.Model):
     blog = models.ForeignKey('Blog', on_delete=models.CASCADE, related_name='comments')
-    content = models.TextField()
+    date = models.DateField(default=timezone.now)
+    name = models.CharField(max_length=100, blank=True)
+    content = models.TextField() # don't want this to blank ever
+    reply = models.TextField(blank=True)
+ 
 
     #related names https://simpleisbetterthancomplex.com/tips/2018/02/10/django-tip-22-designing-better-models.html#naming-your-models
 
