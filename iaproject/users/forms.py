1900cf07cf8765afaa63fe8c2261d3d316a67397
coderbeez <nospam@firville.com>
2020-01-22 20:38:04 +0000

    Create user & profile update forms & add 2 profile template.

--- iaproject/users/forms.py
+++ iaproject/users/forms.py
@@ -1,8 +1,9 @@
 from django import forms
-from django.contrib.auth.models import User #not sure why importing model?
-from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
-# Credit: Corey Scahfer https://www.youtube.com/watch?v=q4jPR-M0TAQ&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=6
+
 
 class UserRegisterForm(UserCreationForm): # form inherits from UserCreationForm
     email = forms.EmailField() # and adds the email field
@@ -13,3 +14,18 @@ class UserRegisterForm(UserCreationForm): # form inherits from UserCreationForm
         #says model that will be affected will be the user model
         model = User #when form validates its going to create a new user
         fields = ['username', 'email', 'password1', 'password2'] #fields we want to be shown and the order
+# Credit: Corey Scahfer https://www.youtube.com/watch?v=q4jPR-M0TAQ&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=6
+
+# a model form is a form that will work with a specific model
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta: #not adding any additional fields so can go straight to meta
+        model = Profile
+        fields = ['image']
+# Credit Corey https://www.youtube.com/watch?v=CQ90L5jfldw&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=9    
