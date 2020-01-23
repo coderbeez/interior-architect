f19e57ee0eb57c26dc90672bd79cf66b6e739c76
coderbeez <nospam@firville.com>
2020-01-23 08:46:59 +0000

    Create blog comment form & add to view & template.

--- iaproject/blog/views.py
+++ iaproject/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Category, Blog, Section, Comment
+from .forms import CommentForm
 
 # Create your views here.
 def blogs(request):
@@ -12,5 +13,21 @@ def blog(request, pk):
     blog = Blog.objects.get(pk=pk)
     sections = Section.objects.filter(blog=blog)
     comments = Comment.objects.filter(blog=blog)
-    context = {'blog': blog, 'sections': sections, 'comments': comments}
+    form = CommentForm()
+    context = {'blog': blog, 'sections': sections, 'comments': comments, 'form': form}
+    
+    
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment (
+                blog = blog,
+                content = form.cleaned_data['content']
+            )
+            comment.save()
+            messages.success(request, f'Comment added')
+            #return redirect('blog')
+            #esle form is not valid??? only thing that could be would be empty contents?
+        
+
     return render(request, 'blog/blog.html', context)
