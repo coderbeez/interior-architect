294fc9efaa008afa3bfee5159ba6f24315653900
coderbeez <nospam@firville.com>
2020-01-25 18:27:01 +0000

    Add first 4 blogs to index

--- iaproject/home/views.py
+++ iaproject/home/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Category, Blog
 
 # Create your views here.
 def index(request):
     #return HttpResponse('Home')
-    return render(request, 'home/index.html')   
+
+    blogs = Blog.objects.filter(exclude=False).order_by('order')[:4]
+    context = {'blogs': blogs}
+
+
+
+
+    return render(request, 'home/index.html', context)
+    # https://stackoverflow.com/questions/6574003/django-limiting-query-results
+
