4855a90ede88b3d7166f2bc2a974a2daf2f6c29a
coderbeez <nospam@firville.com>
2020-01-26 18:46:12 +0000

    Add form & image to contact template

---
+++ iaproject/cv/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.forms import Textarea #only using modelform - this is what django docs import
+#from django.utils.translation import gettext_lazy as _
+#imported to allow label to be blank - from django docs (link below)
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'phone', 'location', 'category', 'query']
+        #labels = {'content': _(''),}
+        #widgets = {'content': Textarea(attrs={'rows':5, 'class': 'textarea-style'}),}
+      
+        
+        #customising model form fields from django docs https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/
+    #Credit: Coding Entrepreneurs Try DJANGO Tutorial - 26 - Form Widgets https://www.youtube.com/watch?v=-oWIyFYyNQw
--- iaproject/cv/views.py
+++ iaproject/cv/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from .models import Contact
+from .forms import ContactForm
 
 # Create your views here.
 def about(request):
@@ -8,7 +11,16 @@ def about(request):
 
 
 def contact(request):
-    #return HttpResponse('CV')
-    return render(request, 'cv/contact.html')   
-  
+    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST) #tried instance=blog and blog=blog
+        if form.is_valid():
+            form.save()
+            #from printout... think there should be a cleaner way to do this
+            messages.success(request, f'Thanks for your query, I will get back shortly!')
+            #form = CommentForm() don't need with redirect
+            return redirect('index')
+    context = {'form': form} 
 
+    return render(request, 'cv/contact.html', context)   
+  
