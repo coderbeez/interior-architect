b3e9e4e0e4faeb197e10f826a6f02e0e5bbaa768
coderbeez <nospam@firville.com>
2020-01-27 15:31:40 +0000

    Add slide functionality to  timeline

--- iaproject/static/js/main.js
+++ iaproject/static/js/main.js
@@ -25,4 +25,43 @@ $window.scroll(function() {
        
       });*/
 
+
+/**----------------------------------------
+    * Accordion
+    ----------------------------------------*/
+
+    /**
+     * Check current state of an accordion target.
+     * Hides a visible target and reveals a hidden target.
+     * Credit: https://stackoverflow.com/questions/178325/how-do-i-check-if-an-element-is-hidden-in-jquery
+     */
+    function slide(target) {
+        if (target.is(":hidden")) {
+            target.slideDown();
+        } else {
+            target.slideUp();
+        }
+    }
+
+    /**
+     * For each level in the accordion allows a button click to slide a target.
+     * Data attribute values allow the association of a button to a target when the template is rendered.
+     * Credit: https://stackoverflow.com/questions/31802861/show-hide-elements-by-data-attribute
+     * https://www.codeproject.com/Questions/369517/how-to-get-data-attributes-in-jquery
+     */
+
+
+    
+    
+     
+    $("[data-btn]").click(function () {
+        console.log('test slider');
+        let value = $(this).attr('data-btn');
+        let target = $('[data-div=' + value + ']');
+        console.log(value);
+        slide(target);
+    });
+
+
+
 });
