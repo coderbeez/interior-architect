9eb6027594a345548ca20deb3660c16eb4ec9ebe
coderbeez <nospam@firville.com>
2020-01-27 12:52:10 +0000

    Remove duplicate import

--- iaproject/blog/views.py
+++ iaproject/blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
-from django.shortcuts import render
 from django.contrib import messages
 from .models import Category, Blog, Section, Comment
 from .forms import CommentForm
