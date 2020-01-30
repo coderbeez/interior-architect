92e56b9657e6071d8b869c2fe3c2bbb04ea33cf8
coderbeez <nospam@firville.com>
2020-01-30 12:29:07 +0000

    Add generic class based views for projects & project

--- iaproject/portfolio/urls.py
+++ iaproject/portfolio/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import ProjectListView, ProjectDetailView #class based views
 from . import views
 
 urlpatterns = [
-    path('', views.projects, name='projects'),
-    path('<pk>', views.project, name='project'),
+    #path('', views.projects, name='projects'),
+    path('', ProjectListView.as_view(), name='projects'),
+    #path('<pk>', views.project, name='project'),
+    path('<int:pk>/', ProjectDetailView.as_view(), name='project'),
 ]
--- iaproject/portfolio/views.py
+++ iaproject/portfolio/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 from .models import Category, Project, Section
+
+# Credit: Class based views Corey https://www.youtube.com/watch?v=-s7e_Fy6NRU&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=10
 
 # Create your views here.
 def projects(request):
-    #return HttpResponse('Portfolio List')
     projects = Project.objects.filter(exclude=False).order_by('order')
     context = {'projects': projects}
     return render(request, 'portfolio/projects.html', context)
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'portfolio/projects.html'
+    context_object_name = 'projects' # what u want it to loop over in template
+    ordering = ['order']
+
 
 def project(request, pk):
     project = Project.objects.get(pk=pk)
@@ -15,3 +23,19 @@ def project(request, pk):
     context = {'project': project, 'sections': sections}
     return render(request, 'portfolio/project.html', context)
 
+class ProjectDetailView(DetailView): #expecting pk by default
+    model = Project
+    template_name = 'portfolio/project.html'
+    context_object_name = 'project' # what u want it to loop over in template
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['sections'] = Section.objects.filter(project=self.object) #OR project=self.kwargs['pk']
+        return context
+
+# Credit https://stackoverflow.com/questions/39652109/django-filtering-inside-of-get-context-data
+
+
+# Credit: Django docs https://docs.djangoproject.com/en/2.2/topics/class-based-views/generic-display/
