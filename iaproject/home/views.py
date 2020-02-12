28e1e7c8fac8bb52a81b7e669784b3ff921246c9
coderbeez <nospam@firville.com>
2020-02-12 10:48:29 +0000

    Choose blogs by likes

--- iaproject/home/views.py
+++ iaproject/home/views.py
@@ -5,8 +5,8 @@ from blog.models import Category, Blog
 
 # Create your views here.
 def index(request):
-    projects = Project.objects.filter(home=True).order_by('order')[:3]
-    blogs = Blog.objects.filter(home=True).order_by('like')[:3]
+    projects = Project.objects.filter(exclude=False, home=True).order_by('order')[:3]
+    blogs = Blog.objects.filter(exclude=False).order_by('like')[:3]
     context = {'title': 'Home', 'projects': projects, 'blogs': blogs}
     return render(request, 'home/index.html', context)
     # https://stackoverflow.com/questions/6574003/django-limiting-query-results
