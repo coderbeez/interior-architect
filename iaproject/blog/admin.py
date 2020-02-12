d7e358bc62f4b81e03f51f1a07e7ae61884ef7c6
coderbeez <nospam@firville.com>
2020-02-12 10:33:56 +0000

    Customise blog admin interface

--- iaproject/blog/admin.py
+++ iaproject/blog/admin.py
@@ -1,18 +1,21 @@
 from django.contrib import admin
 from .models import Category, Blog, Section
 
-# Register your models here.
-
 class SectionInline(admin.TabularInline):
     model = Section
 
 class BlogAdmin(admin.ModelAdmin):
+    list_display = ('order', 'title', 'category', 'like', 'exclude')
+    ordering = ('order',)
+    list_display_links = ('title',)
+    list_filter = ('exclude',)
+    list_editable = ('order', 'exclude')
     inlines = [SectionInline]
-    #from docs
 
 admin.site.register(Category)
 admin.site.register(Blog, BlogAdmin)
 
-
 #https://github.com/s-block/django-nested-inline
 #https://stackoverflow.com/questions/14308050/django-admin-nested-inline
+#from docs & Brad Traversey
+#https://stackoverflow.com/questions/4571916/django-admin-sort-order
