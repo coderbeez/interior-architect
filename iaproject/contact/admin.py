ea3a789ee29bdc9e642c264d6f5aeb85c32e8269
coderbeez <nospam@firville.com>
2020-02-12 16:42:19 +0000

    Customise contact admin

--- iaproject/contact/admin.py
+++ iaproject/contact/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Contact
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category', 'name', 'date', 'exclude')
+    ordering = ('exclude', 'reply', '-id')
+    list_display_links = ('id', 'name')
+    list_filter = ('exclude',)
+    list_editable = ('exclude',)
 
-admin.site.register(Contact)
+admin.site.register(Contact, ContactAdmin)
+#credit brad real estate
