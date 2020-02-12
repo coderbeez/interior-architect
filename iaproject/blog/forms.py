b847decb8b782ab889f48ed9600be9b6a46d9dd9
coderbeez <nospam@firville.com>
2020-02-12 12:13:25 +0000

    Tidy up code & comments

--- iaproject/blog/forms.py
+++ iaproject/blog/forms.py
@@ -1,7 +1,6 @@
 from django import forms
-from django.forms import Textarea #only using modelform - this is what django docs import
-from django.utils.translation import gettext_lazy as _
-#imported to allow label to be blank - from django docs (link below)
+from django.forms import Textarea #this is what django docs import
+from django.utils.translation import gettext_lazy as _ #imported to allow label to be blank - from django docs (link below)
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
@@ -10,11 +9,8 @@ class CommentForm(forms.ModelForm):
         fields = ['content']
         labels = {'content': _(''),}
         widgets = {'content': Textarea(attrs={'rows':5, 'class': 'textarea-style'}),}
-      
-        
-        #customising model form fields from django docs https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/
+    #customising model form fields from django docs https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/
     #Credit: Coding Entrepreneurs Try DJANGO Tutorial - 26 - Form Widgets https://www.youtube.com/watch?v=-oWIyFYyNQw
-
 
 class ReplyForm(forms.ModelForm):
     class Meta:
--- iaproject/blog/urls.py
+++ iaproject/blog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.blogs, name='blogs'),
     path('<pk>', views.blog, name='blog'),
     path('like/<pk>/', views.like, name='like'),
-    path('comments/', views.comments, name='comments'), #why do I have 2 with same name???
+    path('comments/', views.comments, name='comments'),
     path('comments/<pk>/', views.comments, name='comments'),
 ]
