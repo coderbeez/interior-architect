ff5a9b17d9c5b11ddbf384f19ff55ea4a1bc8f2c
coderbeez <nospam@firville.com>
2020-02-15 13:16:12 +0000

    Reverse blog order

--- iaproject/home/views.py
+++ iaproject/home/views.py
@@ -4,7 +4,7 @@ from blog.models import Blog
 
 def index(request):
     projects = Project.objects.filter(exclude=False, home=True).order_by('order')[:3]
-    blogs = Blog.objects.filter(exclude=False).order_by('like')[:3]
+    blogs = Blog.objects.filter(exclude=False).order_by('-like')[:3]
     context = {'title': 'Home', 'projects': projects, 'blogs': blogs}
     return render(request, 'home/index.html', context)
     # https://stackoverflow.com/questions/6574003/django-limiting-query-results
