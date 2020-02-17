d898ed7cb698cd8496f93e8d3332afbc87dd5b5f
coderbeez <nospam@firville.com>
2020-02-17 11:25:41 +0000

    Change order section fields

--- iaproject/portfolio/models.py
+++ iaproject/portfolio/models.py
@@ -25,10 +25,10 @@ class Project(models.Model):
 class Section(models.Model):
     project = models.ForeignKey('Project', on_delete=models.CASCADE, related_name='sections')
     title = models.CharField(max_length=500, blank=True)
+    content = models.TextField(blank=True)
     image = models.ImageField(blank=True, upload_to="project_images")
     caption = models.CharField(max_length=200, blank=True)
     caption_url = models.URLField(max_length=200, blank=True)
-    content = models.TextField(blank=True)
     exclude = models.BooleanField(default=False)
     #related names https://simpleisbetterthancomplex.com/tips/2018/02/10/django-tip-22-designing-better-models.html#naming-your-models
 
