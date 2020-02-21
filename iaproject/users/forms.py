de12dcf25f6a39844b4767731fc859371214debd
coderbeez <nospam@firville.com>
2020-02-21 10:50:55 +0000

    Remove unused user update form

--- iaproject/users/forms.py
+++ iaproject/users/forms.py
@@ -11,13 +11,4 @@ class UserRegisterForm(UserCreationForm): # form inherits from UserCreationForm
         #says model that will be affected will be the user model
         model = User #when form validates its going to create a new user
         fields = ['username', 'email', 'password1', 'password2'] #fields we want to be shown and the order
-# Credit: Corey Scahfer https://www.youtube.com/watch?v=q4jPR-M0TAQ&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=6
-
-# a model form is a form that will work with a specific model
-class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
-
-    class Meta:
-        model = User
-        fields = ['username', 'email']
-# Credit Corey https://www.youtube.com/watch?v=CQ90L5jfldw&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=9    
+# Credit: Corey Scahfer https://www.youtube.com/watch?v=q4jPR-M0TAQ&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=6 
