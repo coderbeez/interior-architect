b15720d4492d0f04c26e708844a63e4ae3b2413e
coderbeez <nospam@firville.com>
2020-02-23 11:22:37 +0000

    Remove console logs & add comments

--- iaproject/static/js/main.js
+++ iaproject/static/js/main.js
@@ -1,16 +1,11 @@
 $(document).ready(function () {
 
-    console.log('test all');
-
-    $('#carouselExampleFade').find('.carousel-item').first().addClass('active');
-/*Credit: https://stackoverflow.com/questions/52870493/carousel-set-first-loop-image-as-active-item*/
-
-    console.log('carousel');
     /**----------------------------------------
-    * Navbar Styling
+    * Navbar
     ----------------------------------------*/
 
     /**
+     * NAVBAR ACTIVE
      * Highlight current page as active on navbar.
      */
     function activeNav() {
@@ -22,9 +17,46 @@ $(document).ready(function () {
     activeNav()
 
 
+    /**
+     * NAVBAR BRAND
+     * Add or remove navbar brand depending on position.
+     * Used only on small devices.
+     * Credit: Modified https://stackoverflow.com/questions/7543718/test-in-jquery-if-an-element-is-at-the-top-of-screen
+     */
+    var distance = $('nav').offset().top,
+        $window = $(window);
+
+    $window.scroll(function () {
+        if ($window.scrollTop() >= distance) {
+            $("[data-nav=navbrand]").show();
+        } else {
+            $("[data-nav=navbrand]").hide();
+        }
+    });
+
+    
+    /**----------------------------------------
+       * Home App
+       ----------------------------------------*/
+
+    /**
+     * CAROUSEL
+     * Set first item in for loop as active.
+     * Credit: https://stackoverflow.com/questions/52870493/carousel-set-first-loop-image-as-active-item
+     */
+    $('#carouselExampleFade').find('.carousel-item').first().addClass('active');
 
 
+    /**----------------------------------------
+    * CV App
+    ----------------------------------------*/
 
+    /**
+     * SKILL CIRCLES
+     * Modified from jquery-circle-progress to use for loop data.
+     * Credit: https://github.com/kottenator/jquery-circle-progress
+     * Credit: Iterator function from https://stackoverflow.com/questions/34949440/how-to-get-data-attribute-value-of-all-elements-using-jquery
+     */
     $('[data-circle]').each(function () {
         $(this).circleProgress({
             value: $(this).attr('data-circle'),
@@ -35,36 +67,9 @@ $(document).ready(function () {
         });
     });
 
-    /*WHERE: Based on code from... modified to use data attributes & use for each*/
-    /*WHERE: .each iterator function from https://stackoverflow.com/questions/34949440/how-to-get-data-attribute-value-of-all-elements-using-jquery*/
-
-
-    var distance = $('nav').offset().top,
-        $window = $(window);
-
-    $window.scroll(function () {
-        if ($window.scrollTop() >= distance) {
-            // Your div has reached the top
-            console.log("test nav");
-            $("[data-nav=navbrand]").show();
-        } else {
-            $("[data-nav=navbrand]").hide();
-        }
-    });
-    //https://stackoverflow.com/questions/7543718/test-in-jquery-if-an-element-is-at-the-top-of-screen
-
-    /*$("nav").scroll(function() {
-        console.log('test');
-        $("[data-nav=navbrand]").show();
-       
-      });*/
-
-
-    /**----------------------------------------
-        * Accordion
-        ----------------------------------------*/
 
     /**
+     * EMPLOYMENT ACCORDION
      * Check current state of an accordion target.
      * Hides a visible target and reveals a hidden target.
      * Credit: https://stackoverflow.com/questions/178325/how-do-i-check-if-an-element-is-hidden-in-jquery
@@ -83,26 +88,13 @@ $(document).ready(function () {
      * Credit: https://stackoverflow.com/questions/31802861/show-hide-elements-by-data-attribute
      * https://www.codeproject.com/Questions/369517/how-to-get-data-attributes-in-jquery
      */
-
-
-
-
-
     $('[data-btn]').click(function () {
-        console.log('test slider');
         let value = $(this).attr('data-btn');
         let target = $('[data-div=' + value + ']');
-        console.log(value);
         $('[data-div]').slideUp(600);
-        console.log('slide up')
-        if (target.is(':hidden')) {
-            console.log('hidden');
-        } else {
-            console.log('not hidden');
-        }
+        if (target.is(':hidden')) {} else {}
         slide(target);
     });
-
 
 
 });
