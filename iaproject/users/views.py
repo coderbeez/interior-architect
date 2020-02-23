989b6e6a73167dcaab2d152e006ad1dd898dc92d
coderbeez <nospam@firville.com>
2020-02-23 09:10:06 +0000

    Remove unused register model, form & template

--- iaproject/users/views.py
+++ iaproject/users/views.py
@@ -1,18 +1,3 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from django.shortcuts import render
 
-# Credit: Corey Schafer https://www.youtube.com/watch?v=q4jPR-M0TAQ&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=6
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST) #intantiate a form with Post data
-        if form.is_valid():
-            form.save() #hashes password etc
-            username = form.cleaned_data.get('username') #python data
-            messages.success(request, f'Account created for {username}! You can now login.')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    context = {'title': 'Register', 'form': form}
-    return render(request, 'users/register.html', context)
+# Create your views here.
