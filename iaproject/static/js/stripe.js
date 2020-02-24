5191d7c9c736495224e7bdff6a75f6fba8633d6b
coderbeez <nospam@firville.com>
2020-02-24 10:18:18 +0000

    Remove then from redirect. Add credits. Tidy up code.

--- iaproject/static/js/stripe.js
+++ iaproject/static/js/stripe.js
@@ -1,23 +1,14 @@
 $(document).ready(function () {
 
 const stripe = Stripe('pk_test_YEXVz6hJlfDMCQfrvKnuAPnG00T43RtZAU');
-const sid = $('[data-sid]').attr('data-sid')
-console.log(sid)
+const sid = $('[data-sid]').attr('data-sid');
 
+ /**
+     * Make the id field from the Checkout Session creation API response available to this file.
+     * Credit: https://stripe.com/docs/payments/checkout/one-time
+     */
 stripe.redirectToCheckout({
-    // Make the id field from the Checkout Session creation API response
-    // available to this file, so you can provide it as parameter here
-    // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
-    sessionId: sid,
-  }).then(function (result) {
-    // If `redirectToCheckout` fails due to a browser or network
-    // error, display the localized error message to your customer
-    // using `result.error.message`.
-    console.log('error')
-  })
+   sessionId: sid,
+  });
 
-console.log('end')
-
-
-
-})
+});
