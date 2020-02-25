cb133508776ccc3bc8280cd8acb379c0a99e1d06
coderbeez <nospam@firville.com>
2020-02-25 16:44:56 +0000

    Save app specific js in app static folder

--- iaproject/static/js/main.js
+++ iaproject/static/js/main.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
 
-    /**----------------------------------------
-    * Navbar
-    ----------------------------------------*/
+    console.log('just navbar js')
 
     /**
      * NAVBAR ACTIVE
@@ -15,7 +13,6 @@ $(document).ready(function () {
     }
 
     activeNav();
-
 
     /**
      * NAVBAR BRAND
@@ -33,68 +30,5 @@ $(document).ready(function () {
             $("[data-nav=navbrand]").hide();
         }
     });
-
-    
-    /**----------------------------------------
-       * Home App
-       ----------------------------------------*/
-
-    /**
-     * CAROUSEL
-     * Set first item in for loop as active.
-     * Credit: https://stackoverflow.com/questions/52870493/carousel-set-first-loop-image-as-active-item
-     */
-    $('#carouselExampleFade').find('.carousel-item').first().addClass('active');
-
-
-    /**----------------------------------------
-    * CV App
-    ----------------------------------------*/
-
-    /**
-     * SKILL CIRCLES
-     * Modified from jquery-circle-progress to use for loop data.
-     * Credit: https://github.com/kottenator/jquery-circle-progress
-     * Credit: Iterator function from https://stackoverflow.com/questions/34949440/how-to-get-data-attribute-value-of-all-elements-using-jquery
-     */
-    $('[data-circle]').each(function () {
-        $(this).circleProgress({
-            value: $(this).attr('data-circle'),
-            size: 80,
-            fill: {
-                gradient: ["#c9c9c9", "#787676"]
-            }
-        });
-    });
-
-
-    /**
-     * EMPLOYMENT ACCORDION
-     * Check current state of an accordion target.
-     * Hides a visible target and reveals a hidden target.
-     * Credit: https://stackoverflow.com/questions/178325/how-do-i-check-if-an-element-is-hidden-in-jquery
-     */
-    function slide(target) {
-        if (target.is(":hidden")) {
-            target.slideDown(600);
-        } else {
-            target.slideUp(600);
-        }
-    }
-
-    /**
-     * For each level in the accordion allows a button click to slide a target.
-     * Data attribute values allow the association of a button to a target when the template is rendered.
-     * Credit: https://stackoverflow.com/questions/31802861/show-hide-elements-by-data-attribute
-     * https://www.codeproject.com/Questions/369517/how-to-get-data-attributes-in-jquery
-     */
-    $('[data-btn]').click(function () {
-        let value = $(this).attr('data-btn');
-        let target = $('[data-div=' + value + ']');
-        $('[data-div]').slideUp(600);
-        if (target.is(':hidden')) {} else {}
-        slide(target);
-    });
-
 
 });
