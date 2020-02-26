3291752e9f3e81236814116d88b7c2fc77559fb9
coderbeez <nospam@firville.com>
2020-02-26 17:17:51 +0000

    Add docstrings & description to cv views

--- iaproject/cv/views.py
+++ iaproject/cv/views.py
@@ -1,8 +1,11 @@
 from django.contrib import messages
 from django.shortcuts import render
-from .models import Role, Point, Example, Skill
+from .models import Role, Skill
 
 def about(request):
+    '''Render about page.
+    Include all roles as jobs or studies, depending on job field.
+    Include all skills.'''
     jobs = Role.objects.filter(job=True).order_by('-order')
     studies = Role.objects.filter(job=False).order_by('-order')
     skills = Skill.objects.all()
