2507e93d5dea0a86e285746bf73648114fb6fc1a
coderbeez <nospam@firville.com>
2020-02-27 12:15:44 +0000

    Pep8 forms

--- iaproject/blog/forms.py
+++ iaproject/blog/forms.py
@@ -1,20 +1,35 @@
 from django import forms
-from django.forms import Textarea #this is what django docs import
-from django.utils.translation import gettext_lazy as _ #imported to allow label to be blank - from django docs (link below)
+from django.forms import Textarea
+''' Get Text Lazy imported to allow label to be blank
+Credit: https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/
+'''
+from django.utils.translation import gettext_lazy as _
 from .models import Comment
 
+
 class CommentForm(forms.ModelForm):
+    '''
+    Credit: Customising model form fields
+    https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/
+    Credit: Form widgets Coding Entrepreneurs
+    https://www.youtube.com/watch?v=-oWIyFYyNQw
+    '''
     class Meta:
         model = Comment
         fields = ['content']
-        labels = {'content': _(''),}
-        widgets = {'content': Textarea(attrs={'rows':5, 'class': 'textarea-style'}),}
-    #customising model form fields from django docs https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/
-    #Credit: Coding Entrepreneurs Try DJANGO Tutorial - 26 - Form Widgets https://www.youtube.com/watch?v=-oWIyFYyNQw
+        labels = {'content': _(''), }
+        widgets = {'content': Textarea(
+            attrs={'rows': 5, 'class': 'textarea-style'}
+        ), }
+
 
 class ReplyForm(forms.ModelForm):
+    '''
+    '''
     class Meta:
         model = Comment
         fields = ['reply', 'exclude']
-        labels = {'reply': _(''), 'exclude': _('exclude'),}
-        widgets = {'reply': Textarea(attrs={'rows':5, 'class': 'textarea-style'}),}
+        labels = {'reply': _(''), 'exclude': _('exclude')}
+        widgets = {'reply': Textarea(
+            attrs={'rows': 5, 'class': 'textarea-style'}
+        ), }
--- iaproject/contact/forms.py
+++ iaproject/contact/forms.py
@@ -1,19 +1,29 @@
 from django import forms
-from django.forms import Textarea #only using modelform - this is what django docs import
+from django.forms import Textarea
+''' GetText Lazy imported to allow label to be blank
+Credit: https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/
+'''
 from django.utils.translation import gettext_lazy as _
-#imported to allow label to be blank - from django docs (link below)
 from .models import Contact
+
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ['name', 'email', 'phone', 'location', 'category', 'query']   
+        fields = ['name', 'email', 'phone', 'location', 'category', 'query']
+
 
 class ReplyForm(forms.ModelForm):
+    '''
+    Credit: Customising model form fields
+    https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/
+    Credit: Form widgets Coding Entrepreneurs
+    https://www.youtube.com/watch?v=-oWIyFYyNQw
+    '''
     class Meta:
         model = Contact
         fields = ['reply', 'exclude']
-        labels = {'reply': _(''), 'exclude': _('exclude'),}
-        widgets = {'reply': Textarea(attrs={'rows':5, 'class': 'textarea-style'}),}
-        #customising model form fields from django docs https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/
-        #Credit: Coding Entrepreneurs Try DJANGO Tutorial - 26 - Form Widgets https://www.youtube.com/watch?v=-oWIyFYyNQw
+        labels = {'reply': _(''), 'exclude': _('exclude'), }
+        widgets = {'reply': Textarea(
+            attrs={'rows': 5, 'class': 'textarea-style'}
+        ), }
